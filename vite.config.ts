75aa92c861ed24a044cdf28937ae7215d337d97c
Jeremy <jeremy@dotech.com.tw>
2023-04-10 17:54:54 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Jy/',
-  plugins: [vue()],
+  plugins: [vue(), pluginRewriteAll()],
   css: {
     preprocessorOptions: {
       scss: {
