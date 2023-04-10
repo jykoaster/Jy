42b9cfe047a4833ce812448bb09a5c816951ee89
jy <ym31215@gmail.com>
2023-04-10 21:58:58 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import pluginRewriteAll from 'vite-plugin-rewrite-all'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import pluginRewriteAll from "vite-plugin-rewrite-all";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Jy/',
+  base: "/",
   plugins: [vue(), pluginRewriteAll()],
   css: {
     preprocessorOptions: {
@@ -13,4 +13,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
